50315c3127243531c84f4383109acc3f818996c8
MZorzy <mzorzy@gmail.com>
2019-11-04 21:38:15 +0100

    Update device_tracker.py

--- custom_components/jlrincontrol/device_tracker.py
+++ custom_components/jlrincontrol/device_tracker.py
@@ -1,72 +1,37 @@
-"""
-Support for Mercedes cars with Mercedes ME.
-For more details about this component, please refer to the documentation at
-https://home-assistant.io/components/device_tracker.mercedesme/
-"""
-import logging
-from datetime import timedelta
 
-from custom_components.mercedesmeapi import DATA_MME
-from homeassistant.helpers.event import track_time_interval
-from homeassistant.util import Throttle
+Learn more
+"""Support for tracking a Volvo."""
+import logging
+
+from homeassistant.components.device_tracker import SOURCE_TYPE_GPS
+from homeassistant.helpers.dispatcher import async_dispatcher_connect
+from homeassistant.util import slugify
+
+from . import DATA_KEY, SIGNAL_STATE_UPDATED
 
 _LOGGER = logging.getLogger(__name__)
 
-DEPENDENCIES = ['mercedesmeapi']
 
-MIN_TIME_BETWEEN_SCANS = timedelta(seconds=30)
-
-
-def setup_scanner(hass, config, see, discovery_info=None):
-    """Set up the Mercedes ME tracker."""
+async def async_setup_scanner(hass, config, async_see, discovery_info=None):
+    """Set up the Volvo tracker."""
     if discovery_info is None:
-        return False
+        return
 
-    data = hass.data[DATA_MME].data
+    p = v.get_position()
+    gps_position = (p['position']['latitude'], p['position']['longitude'])
 
-    if not data.cars:
-        return False
+    async def see_vehicle():
+        """Handle the reporting of the vehicle position."""
+        host_name = instrument.vehicle_name
+        dev_id = "volvo_{}".format(slugify(host_name))
+        await async_see(
+            dev_id=dev_id,
+            host_name=host_name,
+            source_type=SOURCE_TYPE_GPS,
+            gps=gps_position,
+            icon="mdi:car",
+        )
 
-    MercedesMEDeviceTracker(hass, config, see, data)
+    async_dispatcher_connect(hass, SIGNAL_STATE_UPDATED, see_vehicle)
 
     return True
-
-
-class MercedesMEDeviceTracker(object):
-    """A class representing a Mercedes ME device tracker."""
-
-    def __init__(self, hass, config, see, data):
-        """Initialize the Mercedes ME device tracker."""
-        self.see = see
-        self.data = data
-        self.update_info()
-
-        track_time_interval(
-            hass, self.update_info, MIN_TIME_BETWEEN_SCANS)
-
-    @Throttle(MIN_TIME_BETWEEN_SCANS)
-    def update_info(self, now=None):
-        """Update the device info."""
-        for device in self.data.cars:
-
-            location = device.location
-            if location is None:
-                continue
-
-            dev_id = device.finorvin
-            name = device.licenseplate
-
-            p = v.get_position()
-            position = (p['position']['latitude'], p['position']['longitude'])
-
-            attrs = {
-                'trackr_id': dev_id,
-                'id': dev_id,
-                'name': name
-            }
-            self.see(
-                dev_id=dev_id, host_name=name,
-                gps=position, attributes=attrs
-            )
-
-        return True
